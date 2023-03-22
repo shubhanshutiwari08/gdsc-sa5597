396ef89afa05587848b7f31cf5a1863ad00a353a
shubhanshutiwari08 <tiwarishubhanshu1071@gmail.com>
2023-03-22 15:53:13 +0530

    login authentication created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
+import jwt_decode from "jwt-decode";
 import './App.css';
 
 function App() {
+  const google = window.google;
+  function handleResponse(response){
+    console.log("Encoded JWT ID token :"+ response.credential);
+    var user =jwt_decode(response.credential);
+    console.log(user);
+  };
+
+
+  useEffect(()=>{
+    // global google 
+    google.accounts.id.initialize({
+      client_id: "344326936290-65lj4u1erq1atenou2ho2t49c69vfsql.apps.googleusercontent.com",
+      callback: handleResponse
+    })
+  });
+
+  google.accounts.id.renderButton(
+    document.getElementById("signin"),
+    {theme:"outline",size:"large"}
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello</h1>
+      <div id="signin"></div>
     </div>
   );
 }
